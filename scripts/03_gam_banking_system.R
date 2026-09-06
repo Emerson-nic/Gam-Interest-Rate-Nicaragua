a26c6c6343d5f8016d7de9172da24881ceef4feb
Emerson-nic <emersonlzeledon@outlook.com>
2026-09-06 13:30:26 -0600

    gam starting

---
+++ scripts/03_gam_banking_system.R
@@ -0,0 +1,10 @@
+#load library ---- 
+options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2026-09-04"))
+#this install 2026-09-03 librery
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(tidyverse,
+               mgcv
+)
+
+banca <- readr::read_csv("csv/sistema_bancario.csv")
